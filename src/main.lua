ec927c50c5271614f5dabd251758924c0bbe24dd
Tommaso Paoli <paoli7612@gmail.com>
2018-04-16 13:12:47 +0200

    update builder of wall

--- src/main.lua
+++ src/main.lua
@@ -5,18 +5,19 @@ Wall = require("wall")
 Npc = require("npc")
 Group = require("group")
 
+
 function load_walls()
-	for i = 0,grill.tile_w do
-		wall = Wall(grill,0,i)
-		all_sprites.add(wall)
-		wall = Wall(grill,grill.tile_h-1,i)
+	function new_wall(x,y)
+		wall = Wall(grill,x,y)
 		all_sprites.add(wall)
 	end
+	for i = 0,grill.tile_w do
+		new_wall(0,i)
+		new_wall(grill.tile_h-1,i)
+	end
 	for i = 0,grill.tile_h do
-		wall = Wall(grill,i,0)
-		all_sprites.add(wall)
-		wall = Wall(grill,i,grill.tile_w-1)
-		all_sprites.add(wall)
+		new_wall(i,0)
+		new_wall(i,grill.tile_w-1)
 	end
 end
 
--- src/npc.lua
+++ src/npc.lua
@@ -8,6 +8,7 @@ function Npc(grill,x,y)
 		y=y,
 		size=grill.tile
 	}
+  
 	function npc.draw()
 		love.graphics.setColor(0, 0, 255)
 		love.graphics.rectangle("fill", npc.x*npc.size, npc.y*npc.size, npc.size, npc.size)
