be170a00b9ac0e7058557502932b59062e20b7de
Tommaso Paoli <paoli7612@gmail.com>
2018-04-16 13:06:28 +0200

    create contourn of walls

--- src/main.lua
+++ src/main.lua
@@ -4,18 +4,27 @@ Player = require("player")
 Wall = require("wall")
 Group = require("group")
 
+function load_walls()
+	for i = 0,grill.tile_w do
+		wall = Wall(grill,0,i)
+		all_sprites.add(wall)
+		wall = Wall(grill,grill.tile_h-1,i)
+		all_sprites.add(wall)
+	end
+	for i = 0,grill.tile_h do
+		wall = Wall(grill,i,0)
+		all_sprites.add(wall)
+		wall = Wall(grill,i,grill.tile_w-1)
+		all_sprites.add(wall)
+	end
+end
+
+
 function love.load()
 	grill = Grill()
 	all_sprites = Group()
 	player = Player(grill,5,5)
-	for i = 0,10 do
-		wall = Wall(grill,0,i)
-		all_sprites.add(wall)
-	end
-	for i = 0,10 do
-		wall = Wall(grill,i,0)
-		all_sprites.add(wall)
-	end
+	load_walls()
 	player.group = all_sprites
 	all_sprites.show()
 
