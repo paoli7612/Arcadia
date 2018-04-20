da6d8645e067bcbd4a7fd6220351b1a147912b91
Tommaso Paoli <paoli7612@gmail.com>
2018-04-20 17:38:40 +0200

    load images

--- map_editor/main.py
+++ map_editor/main.py
@@ -8,15 +8,21 @@ class Game:
         self.screen = pygame.display.set_mode((self.opt.SIZE))
         pygame.display.set_caption(self.opt.TITLE)
         self.path = os.path.dirname(__file__)
-        self.path_maps = os.path.join(self.path, ".." , "src", "maps")
-        self.path_img = os.path.join(self.path, ".." , "src", "img")
         self.load_map()
         self.load_images()
 
     def load_images(self):
-        print(os.listdir(self.path_img))
+        self.path_img = os.path.join(self.path, ".." , "src", "img")
+        self.images = os.listdir(self.path_img)
+        for image in self.images:
+            print(image)
+            image = pygame.image.load(os.path.join(self.path_img, image))
+            self.screen.blit(image, (0,0))
+            pygame.display.flip()
+            time.sleep(1)
 
     def load_map(self):
+        self.path_maps = os.path.join(self.path, ".." , "src", "maps")
         self.map_file = open(os.path.join(self.path_maps, self.name_map)
                         + self.opt.LUA_FORMAT, "r")
         print(self.map_file.read())
