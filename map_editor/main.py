b3d3dfaf0858ceef1ebfd51e45c7dc5f237b689f
Tommaso Paoli <paoli7612@gmail.com>
2018-04-20 17:31:43 +0200

    get list of images

--- map_editor/main.py
+++ map_editor/main.py
@@ -8,17 +8,22 @@ class Game:
         self.screen = pygame.display.set_mode((self.opt.SIZE))
         pygame.display.set_caption(self.opt.TITLE)
         self.path = os.path.dirname(__file__)
-        self.path_maps = os.path.join(self.path, ".." , "src", "img")
-        self.load_map(self.name_map)
+        self.path_maps = os.path.join(self.path, ".." , "src", "maps")
+        self.path_img = os.path.join(self.path, ".." , "src", "img")
+        self.load_map()
+        self.load_images()
 
-    def load_map(self, name_map):
-        print(os.path.join(self.path_maps, name_map) + self.opt.LUA_FORMAT)
+    def load_images(self):
+        print(os.listdir(self.path_img))
+
+    def load_map(self):
+        self.map_file = open(os.path.join(self.path_maps, self.name_map)
+                        + self.opt.LUA_FORMAT, "r")
+        print(self.map_file.read())
 
 
 
 # test
-
-
 if __name__ == "__main__":
     import sys
     g = Game(sys.argv)
