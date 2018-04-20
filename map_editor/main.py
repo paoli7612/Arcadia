14f84ab04f39feace4a96ffdf74e27ae31d325dc
Tommaso Paoli <paoli7612@gmail.com>
2018-04-20 17:27:08 +0200

    correct argv

--- map_editor/main.py
+++ map_editor/main.py
@@ -2,18 +2,26 @@ import pygame, time, os
 from setting import Setting
 
 class Game:
-    def __init__(self):
+    def __init__(self, argv):
         self.opt = Setting()
+        self.name_map = argv[1]
         self.screen = pygame.display.set_mode((self.opt.SIZE))
         pygame.display.set_caption(self.opt.TITLE)
-        self.path = os.getcwd()
+        self.path = os.path.dirname(__file__)
         self.path_maps = os.path.join(self.path, ".." , "src", "img")
+        self.load_map(self.name_map)
 
     def load_map(self, name_map):
         print(os.path.join(self.path_maps, name_map) + self.opt.LUA_FORMAT)
 
-g = Game()
-g.load_map("village")
-g.load_map("castle")
-g.load_map("spawn")
-time.sleep(2)
+
+
+# test
+
+
+if __name__ == "__main__":
+    import sys
+    g = Game(sys.argv)
+    time.sleep(2)
+
+# now we start with "python main.py [map_name]"
