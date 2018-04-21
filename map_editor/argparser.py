e44d6aa270b301ad500241512a5cc753b7bfef5b
Tommaso Paoli <paoli7612@gmail.com>
2018-04-21 22:39:24 +0200

    add description of argparser

--- map_editor/argparser.py
+++ map_editor/argparser.py
@@ -3,6 +3,11 @@ import argparse
 
 description = """
         MAP EDITOR
+        This simply program allow you to create or change
+        all game map. All game map saved in maps folder.
+        If you give this program a name of a exists map, the program
+        allow you to change this. Else the program create a new map
+        with this new name.
 """
 class Argparser:
     def __init__(self):
