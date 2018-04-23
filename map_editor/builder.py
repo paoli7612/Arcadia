923f04cd4aa51b89dba73462806897b5e68706f0
Tommaso Paoli <paoli7612@gmail.com>
2018-04-23 15:05:59 +0200

    update builder

--- map_editor/builder.py
+++ map_editor/builder.py
@@ -13,39 +13,23 @@ class Builder:
         data = str()
         for y in self.program.map.matrix:
             for x in y:
-                if x < 10: x = "0"+str(x)
+                if x < 10: x = "0" + str(x)
                 data+=",%s"%str(x)
             data+="\n\t\t\t\t\t\t"
-        data = data[1:]
+        data = data[1:] # remove first ","
+
         down = "\n\t\t\t\t\t\t\t\t\t\t"
-        # WALL
-        walls = str()
-        for wall in self.program.converter.properties["walls"]:
-            walls += (template.walls %(wall["id"],wall["color"],wall["type"]+1)) + down
-        # FLOOR
-        floors = str()
-        for floor in self.program.converter.properties["floors"]:
-            floors += (template.floors %(floor["id"],floor["bloke"],floor["type"]+1)) + down
-        # DOOR
-        doors = str()
-        for door in self.program.converter.properties["doors"]:
-            doors += (template.doors %(door["id"],door["dest"],door["coord_x"],door["coord_y"],door["type"]+1)) + down
-        # NPC
-        npcs = str()
-        for npc in self.program.converter.properties["npc"]:
-            npcs += (template.npc %(npc["type"],npc["coord_x"],npc["coord_y"],npc["offset_x"],npc["offset_y"])) + down
-        # DECOR
-        decors = str()
-        for decor in self.program.converter.properties["decor"]:
-            decors += (template.decor %(decor["type"],decor["coord_x"],decor["coord_y"])) + down
-        # TORCH
-        torchs = str()
-        for torch in self.program.converter.properties["torch"]:
-            torchs += (template.torch %(torch["color"],torch["coord_x"],torch["coord_y"])) + down
-        # CARTEL
-        cartels = str()
-        for cartel in self.program.converter.properties["cartel"]:
-            cartels += (template.cartel %(cartel["text"],cartel["coord_x"],cartel["coord_y"])) + down
+        walls, floors, doors, npcs, decors, torchs, cartels = str(),str(),str(),str(),str(),str(),str()
+
+        props = self.program.converter.properties
+        for wall in props: walls.append((template.walls %(wall["id"],wall["color"],wall["type"]+1)) + down)
+        for floor in props: floors.append((template.floors %(floor["id"],floor["bloke"],floor["type"]+1)) + down)
+        for door in props: doors.append((template.doors %(door["id"],door["dest"],door["coord_x"],door["coord_y"],door["type"]+1)) + down)
+        for npc in props: npcs += (template.npc %(npc["type"],npc["coord_x"],npc["coord_y"],npc["offset_x"],npc["offset_y"])) + down
+        for decor in props: decors += (template.decor %(decor["type"],decor["coord_x"],decor["coord_y"])) + down
+        for torch in props: torchs += (template.torch %(torch["color"],torch["coord_x"],torch["coord_y"])) + down
+        for cartel in props: cartels += (template.cartel %(cartel["text"],cartel["coord_x"],cartel["coord_y"])) + down
+
         f = open(self.program.path_maps, "w")
-        f.write(template.to_fill %(self.program.name_map, data,walls,floors,doors,npcs,decors,torchs,cartels))
+        f.write(template.to_fill %(self.program.name_map, walls, floors, doors, npcs, decors, torchs, cartels))
         f.close()
