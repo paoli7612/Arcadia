d2c511b8c3008b5dcc7a4c98ae220c3869e7c46c
Tommaso Paoli <paoli7612@gmail.com>
2018-04-29 22:20:22 +0200

    update readme

--- src/interface.lua
+++ src/interface.lua
@@ -1,10 +1,56 @@
 -- interface.lua
 
 function Interface(boss)
+  local grill = boss.grill
+  local attr = {
+      life = 100,
+      life_max = 100,
+      level = 1,
+      money = 0,
+      exp = 0,
+      exp_max = 50
+    }
   local interface = {
 
   }
+
+function interface.draw()
+  love.graphics.setColor(255,255,0)
+  love.graphics.print("Money " .. attr.money, grill.WIDTH - grill.tile*4, 0)
+
+  love.graphics.setColor(255,0,255)
+  love.graphics.print("Exp " .. attr.exp .. "/" .. attr.exp_max, grill.WIDTH - grill.tile*4, grill.tile)
+
+  love.graphics.setColor(255,255,255)
+  love.graphics.print("Level " .. attr.level, grill.WIDTH - grill.tile*3 * 3, 0)
+
+  love.graphics.setColor(255,0,0)
+  love.graphics.print("Life " .. attr.life .. "/" .. attr.life_max, grill.WIDTH - grill.tile*3 * 3, grill.tile)
+
+  love.graphics.setColor(255, 255, 255)
+end
+
+function interface.add_life(life)
+  attr.life = attr.life + life
+  if attr.life > attr.life_max then
+    attr.life = attr.life_max end
+  if attr.life <= 0 then
+    print("dead") end
+end
+
+function interface.add_exp(exp)
+  attr.exp = attr.exp + exp
+  if attr.exp >= attr.exp_max then
+    attr.exp = attr.exp - attr.exp_max
+    attr.exp_max = attr.exp_max * 2
+    attr.level = attr.level + 1
+    attr.life_max = attr.life_max + 10
+    print("new level")
+  end
+end
+
+
   return interface
 end
 
-return interface
+return Interface
--- src/inventory.lua
+++ src/inventory.lua
@@ -1,14 +1,11 @@
 -- inventory.lua
 
+Interface = require("interface")
+
 function Inventory(boss)
   local inventory = {}
   local bag = {}
-
-  local money = 0
-  local life = 100
-  local exp = 0
-  local level = 1
-
+  local interface = Interface(boss)
   local grill = boss.grill
   local show = false
 
@@ -18,20 +15,7 @@ function Inventory(boss)
       love.graphics.rectangle("fill", 0, 0, grill.WIDTH, grill.tile*2,0,2,2)
       for i,p in ipairs(bag) do
         p.draw(i,0) end
-
-      love.graphics.setColor(255,255,0)
-      love.graphics.print("Money " .. money, grill.WIDTH - grill.tile*4, 0)
-
-      love.graphics.setColor(255,0,255)
-      love.graphics.print("Exp " .. exp, grill.WIDTH - grill.tile*4, grill.tile)
-
-      love.graphics.setColor(255,255,255)
-      love.graphics.print("Level " .. level, grill.WIDTH - grill.tile*3 * 3, 0)
-
-      love.graphics.setColor(255,0,0)
-      love.graphics.print("Life " .. life, grill.WIDTH - grill.tile*3 * 3, grill.tile)
-
-      love.graphics.setColor(255, 255, 255)
+      interface.draw()
     end
   end
 
