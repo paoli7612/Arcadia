c7a5bd9c5fec651dd54f252a22910adb86cc57e6
Tommaso Paoli <paoli7612@gmail.com>
2018-05-02 20:24:06 +0200

    tilesheet_editor main creation

---
+++ tilesheet_editor/main.py
@@ -0,0 +1,34 @@
+import os, re
+
+class Program:
+    def __init__(self):
+        self.sprites_list = list()
+        self.path = os.path.dirname(__file__)
+        name_file = "s.json"
+        self.json_path = os.path.join(self.path, "..", "src", "img", name_file)
+        self.json_text = self.get_text(self.json_path)
+        self.start()
+
+    def start(self):
+        for i in re.findall('"[A-Za-z]*[0-9][0-9]+"', self.json_text):
+            id = i.replace('"','')
+            id = int(id)
+            try:
+                if id  in self.sprites_list:
+                    self.sprites_list.append(id_1 + 1)
+                    id_1 = id_1 + 1
+                    print(id_1)
+                else:
+                    id_1 = id
+                    self.sprites_list.append(id)
+            except: pass
+        print(self.sprites_list)
+
+    def get_text(self,file_name):
+        string = str()
+        for row in open(file_name, "r"):
+            string += row.strip()
+        return string
+
+if __name__ == "__main__":
+    p = Program()
