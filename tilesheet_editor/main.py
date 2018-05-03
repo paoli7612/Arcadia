c257dae833f4a1cc85482b75ae96cb26462ae191
boss <federigus04@gmail.com>
2018-05-03 21:21:21 +0200

    ordine correto

--- tilesheet_editor/main.py
+++ tilesheet_editor/main.py
@@ -11,8 +11,10 @@ class Program:
         self.start()
 
     def start(self):
+        z_id=""
         for i in re.findall('"[A-Za-z]*[0-9][0-9]+"', self.json_text):
             id = i.replace('"','')
+            head_id = id[0]
             id = int(id[1:])
             try:
                 self.sprites_list.append(id)
@@ -21,11 +23,23 @@ class Program:
                 elif self.sprites_list[-2] < id  and self.sprites_list[-1]!=1:
                     self.sprites_list[-1] = self.sprites_list[-2]+1
             except: pass
+
+
+            zz = 4-len(str(self.sprites_list[-1]))
+            for z in range(zz):
+                z_id = z_id + '0'
+            print(head_id+z_id+str(self.sprites_list[-1]))
+            z_id =''
     def get_text(self,file_name):
         string = str()
         for row in open(file_name, "r"):
             string += row.strip()
         return string
+
+
+
+
+
 
 if __name__ == "__main__":
     a = Argparser()
