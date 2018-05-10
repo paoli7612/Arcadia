9149f9b4a3af92c874eac173b2a19882d2a7d7ad
Tommaso Paoli <paoli7612@gmail.com>
2018-05-10 18:33:09 +0200

    update street

---
+++ src/descriptions/jeff.lua
@@ -0,0 +1,13 @@
+return {
+  name = "andy",
+  quests = {
+  },
+  speak = {
+    {"ciao io sono jeff", "asd asd asd"},
+    {"blablabla", "bla bala lasd"},
+    {"asdfasfas","asdasf","fasdfasds"},
+    {"adsa","asdasf","fasdfasds"},
+    {"asdfasssfas","asdasf","fasdfajhgjgsds"},
+    {"asdfasfdsdsas","fghfg","fasdfasds"}
+  }
+}
