43e23d6fd791cee2360129ced5dfec23a0a68d58
Tommaso Paoli <paoli7612@gmail.com>
2018-05-13 11:12:28 +0200

    update folder

--- src/gui/interface.lua
+++ src/gui/interface.lua
@@ -10,51 +10,48 @@ function Interface(boss)
       exp = 0,
       exp_max = 50
     }
-  local interface = {
+  local interface = {}
 
-  }
+  function interface.draw()
+    love.graphics.setColor(255,255,0)
+    love.graphics.print("Money " .. attr.money, grill.WIDTH - grill.tile*4, 0)
 
-function interface.draw()
-  love.graphics.setColor(255,255,0)
-  love.graphics.print("Money " .. attr.money, grill.WIDTH - grill.tile*4, 0)
+    love.graphics.setColor(255,0,255)
+    love.graphics.print("Exp " .. attr.exp .. "/" .. attr.exp_max, grill.WIDTH - grill.tile*4, grill.tile)
 
-  love.graphics.setColor(255,0,255)
-  love.graphics.print("Exp " .. attr.exp .. "/" .. attr.exp_max, grill.WIDTH - grill.tile*4, grill.tile)
+    love.graphics.setColor(255,255,255)
+    love.graphics.print("Level " .. attr.level, grill.WIDTH - grill.tile*3 * 3, 0)
 
-  love.graphics.setColor(255,255,255)
-  love.graphics.print("Level " .. attr.level, grill.WIDTH - grill.tile*3 * 3, 0)
+    love.graphics.setColor(255,0,0)
+    love.graphics.print("Life " .. attr.life .. "/" .. attr.life_max, grill.WIDTH - grill.tile*3 * 3, grill.tile)
 
-  love.graphics.setColor(255,0,0)
-  love.graphics.print("Life " .. attr.life .. "/" .. attr.life_max, grill.WIDTH - grill.tile*3 * 3, grill.tile)
-
-  love.graphics.setColor(255, 255, 255)
-end
-
-function interface.add_life(life)
-  attr.life = attr.life + life
-  if attr.life > attr.life_max then
-    attr.life = attr.life_max end
-  if attr.life <= 0 then
-    print("dead") end
-end
-
-function interface.add_money(money)
-  attr.money = attr.money + money
-end
-
-
-function interface.add_exp(exp)
-  attr.exp = attr.exp + exp
-  if attr.exp >= attr.exp_max then
-    -- level up
-    attr.exp = attr.exp - attr.exp_max
-    attr.exp_max = attr.exp_max * 2
-    attr.level = attr.level + 1
-    attr.life_max = attr.life_max + 10
-    attr.life = attr.life_max
-    print("new level")
+    love.graphics.setColor(255, 255, 255)
   end
-end
+
+  function interface.add_life(life)
+    attr.life = attr.life + life
+    if attr.life > attr.life_max then
+      attr.life = attr.life_max end
+    if attr.life <= 0 then
+      print("dead") end
+  end
+
+  function interface.add_money(money)
+    attr.money = attr.money + money
+  end
+
+  function interface.add_exp(exp)
+    attr.exp = attr.exp + exp
+    if attr.exp >= attr.exp_max then
+      -- level up
+      attr.exp = attr.exp - attr.exp_max
+      attr.exp_max = attr.exp_max * 2
+      attr.level = attr.level + 1
+      attr.life_max = attr.life_max + 10
+      attr.life = attr.life_max
+      print("new level")
+    end
+  end
 
 
   return interface
