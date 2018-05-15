ac05bdb1b22a9a4a544ed56093e593c8ba681191
Tommaso Paoli <paoli7612@gmail.com>
2018-05-15 18:40:21 +0200

    create a python script to reset the save of game

---
+++ save/reset.py
@@ -0,0 +1,25 @@
+import os, shutil, random, time
+
+def reset():
+    path = os.path.dirname(__file__)
+    folder = os.path.join(path, "init")
+    files = os.listdir(folder)
+    for file in files:
+        src = os.path.join(folder, file)
+        shutil.copyfile(src, file)
+
+def get_new_code():
+    code = str()
+    for n in range(5):
+        code += str(random.randint(0,9))
+    return code
+
+if __name__ == "__main__":
+    code = get_new_code()
+    print("Cancellare tutti i salvataggi di gioco?")
+    print("Questa operazione non e' reversibile")
+    print("Digitare il seguente codice e premere invio per confermare")
+    print("%s" %code)
+    if input("\n -> ") == code: reset(); print("Dati di gioco cancellati")
+    else: print("Dati di gioco NON cancellati")
+    time.sleep(2)
