a74160b255d9355e7f77d7855544c957f158e2ae
Tommaso Paoli <paoli8712@gmail.com>
2020-01-16 17:06:16 +0100

    new maps

--- src/maps/curie6-0.lua
+++ src/maps/curie6-0.lua
@@ -344,8 +344,14 @@ return {
 										{dest = "curie5-0", code = "30007", coord_x = 8, coord_y = 11, dest_x = 31, dest_y = 13, id = 3},
 										{dest = "curie5-0", code = "30007", coord_x = 8, coord_y = 12, dest_x = 31, dest_y = 14, id = 3},
 										{dest = "curie5-0", code = "30007", coord_x = 8, coord_y = 13, dest_x = 31, dest_y = 14, id = 3},
-                    {dest = "curie6-0(1)", code = "30007", coord_x = 13, coord_y = 18, dest_x = 13, dest_y = 19, id = 5},
+										{dest = "curie6-0(1)", code = "30007", coord_x = 13, coord_y = 18, dest_x = 13, dest_y = 19, id = 5},
 										{dest = "curie6-0(1)", code = "30007", coord_x = 14, coord_y = 18, dest_x = 14, dest_y = 19, id = 5},
+										{dest = "curie6-0(1)", code = "30007", coord_x = 25, coord_y = 18, dest_x = 25, dest_y = 18, id = 6},
+										{dest = "curie6-0(1)", code = "30007", coord_x = 26, coord_y = 18, dest_x = 26, dest_y = 18, id = 6},
+                    {dest = "curie7-0", code = "30007", coord_x = 31, coord_y = 10, dest_x = 2, dest_y = 10, id = 4},
+                    {dest = "curie7-0", code = "30007", coord_x = 31, coord_y = 11, dest_x = 2, dest_y = 10, id = 4},
+                    {dest = "curie7-0", code = "30007", coord_x = 31, coord_y = 12, dest_x = 2, dest_y = 10, id = 4},
+										{dest = "curie7-0", code = "30007", coord_x = 31, coord_y = 13, dest_x = 2, dest_y = 10, id = 4},
 
     			},
     			npc={
---
+++ src/maps/curie7-0.lua
@@ -0,0 +1,37 @@
+return {
+    	name = "curie7-0",
+    	properties={
+    			wall={
+                    
+    			},
+    			floor={
+                    
+    			},
+    			door={
+                    {dest = "curie6-0", code = "30006", coord_x = 6, coord_y = 9, dest_x = 18, dest_y = 18, id = 1},
+										
+    			},
+    			npc={
+                    
+                },
+    			decor={
+                    
+                },
+    			torch={
+                    
+    			},
+    			cartel={
+                    
+                },
+                water={
+                    
+                },
+                lever={
+                    
+                },
+                chest={
+                    
+                }
+
+    		}
+    	}
