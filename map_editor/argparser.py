a565bfa1c6d6d4af8b0dde25b4a41219658126be
paoli7612 <paoli7612@gmail.com>
2020-11-15 13:19:10 +0100

    UPDATE 3

--- map_editor/argparser.py
+++ map_editor/argparser.py
@@ -1,17 +1,10 @@
-import sys
+import sys, os
 
-description = """
-        MAP EDITOR
-        This simply program allow you to create or change
-        all game map. All game map saved in maps folder.
-        If you give this program a name of a exists map, the program
-        allow you to change this. Else the program create a new map
-        with this new name.
-"""
 class Argparser:
     def __init__(self):
         if len(sys.argv) == 2:
             self.name_map = sys.argv[1]
         else:
-            print(description)
+            for map in os.listdir(os.path.join(os.path.dirname(__file__), ".." , "src", "maps")):
+                print('\t' + map.split('.lua')[0])
             self.name_map = input("Inserire il nome della mappa\n-> ")
