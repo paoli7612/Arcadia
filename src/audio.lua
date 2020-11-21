4a67f968f1fce914ebb736dbd575550944c1f18e
paoli7612 <paoli7612@gmail.com>
2020-11-21 17:51:15 +0100

    change song in map

--- src/audio.lua
+++ src/audio.lua
@@ -1,11 +1,16 @@
 function Audio()
   local audio = {}
   audio.song = 'spawn'
+  audio.music = love.audio.newSource("snd/spawn.wav", 'stream')
 
   function audio.change_map(name)
     if not (audio.song == name) then
-      print("Cambio canzone: "..name)
-      audio.song = name
+      if love.filesystem.exists("snd/"..name..".wav") then
+        audio.song = name
+        audio.music:stop()
+        audio.music = love.audio.newSource("snd/"..name..".wav", 'stream')
+        audio.music:play()
+      end
     end
   end
 
